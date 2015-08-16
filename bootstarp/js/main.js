e9603e1afd28eae2668737b871870d481b0d393d
coldsky <style177@126.com>
2015-08-16 16:02:07 +0800

    bootstrap project

---
+++ bootstarp/js/main.js
@@ -0,0 +1,23 @@
+
+var themeApp = {
+	
+	backToTop: function() {
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 100) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		$('#back-to-top').on('click', function(e){
+			e.preventDefault();
+			$('html, body').animate({scrollTop : 0},1000);
+			return false;
+		});
+	}
+}
+
+
+$(document).ready(function(){
+  themeApp.backToTop();
+});
